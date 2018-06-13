f08561530a9562e98fde54e8ec7490723fabcda2
Mayank Sharma <cinegogwl@gmail.com>
2018-06-13 18:15:41 +0530

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,33 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+    if(err){
+    return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    
+// db.collection('Todos').find({_id:new ObjectID('5b20daef77c90db0789ce59b')}).toArray().then((docs) => {
+//        console.log('Todos');
+//        console.log(JSON.stringify(docs,undefined,2));
+//    },(err)=>{
+//        console.log('Unable to fetch todos',err);
+//    });
+//    
+//     db.collection('Users').find({}).count().then((count) => {
+//        console.log(`Todos count: ${count}`);
+////        console.log(JSON.stringify(docs,undefined,2));
+//    },(err)=>{
+//        console.log('Unable to fetch todos',err);
+//    });
+    
+        
+     db.collection('Users').find({name:'Mayank'}).count().then((count) => {
+        console.log(`Users count: ${count}`);
+//        console.log(JSON.stringify(docs,undefined,2));
+    },(err)=>{
+        console.log('Unable to fetch todos',err);
+    });
+    
+//     db.close();
+});
